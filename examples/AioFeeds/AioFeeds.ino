a6d9060b915e2cbdb2bea03b29e0adad7414e035
Imre Horvath <imi.horvath@gmail.com>
2016-12-23 19:44:51 +0100

    Add AOI feed parsing with ArduinoJson example

--- examples/AioFeeds/AioFeeds.ino
+++ examples/AioFeeds/AioFeeds.ino
@@ -7,17 +7,22 @@
   Works only with Yun and its clones like the Seeeduino Cloud, but not with
   an Uno or other Arduinos.
 
+  Dependencies: ArduinoJson library
+
   Usage:
-  1. Replace the placeholder XXXs below with your actual AIO KEY
-  2. Upload the sketch using a USB cable.
-  3. Wait for the RED LED to light up on the board.
-  4. Open the serial monitor in the Arduino IDE.
+  1. Make sure you've installed the ArduinoJson library in the Library Manager
+  2. Replace the placeholder XXXs below with your actual AIO KEY
+  3. Upload the sketch using a USB cable.
+  4. Wait for the RED LED to light up on the board.
+  5. Open the serial monitor in the Arduino IDE.
 
   Written by Imre Horvath, 2016
  */
 
 #include <Bridge.h>
 #include <BridgeHttpClient.h>
+
+#include <ArduinoJson.h>
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -41,10 +46,14 @@ void setup() {
   // Adafruit IO REST API call
   client.get("https://io.adafruit.com/api/feeds");
 
+  // Collect the response body into this string for parsing
+  String json;
+
   // Dump the response body
   SerialUSB.println("Response Body:");
   while (client.available() > 0) {
     char c = client.read();
+    json += c;
     SerialUSB.print(c);
   }
 
@@ -52,13 +61,22 @@ void setup() {
   SerialUSB.print("Response code: ");
   SerialUSB.println(client.getResponseCode());
 
-  // Print a response header value
-  String server;
-  if (client.getResponseHeaderValue("Server", server)) {
-    SerialUSB.print("Header \"Server\" has value: ");
-    SerialUSB.println(server);
-  } else {
-    SerialUSB.println("Header \"Server\" not found");
+  // Parse the list of feeds and print the name and ids
+
+  DynamicJsonBuffer jsonBuffer;
+
+  JsonArray& array = jsonBuffer.parseArray(json);
+  if (!array.success()) {
+    SerialUSB.println("parseArray() failed");
+    while (1) {}
+  }
+
+  for (JsonArray::iterator it = array.begin(); it != array.end(); ++it) {
+    JsonObject& feed = *it;
+    const char* name = feed["name"];
+    const char* id = feed["id"];
+    SerialUSB.print("Feed name: "); SerialUSB.println(name);
+    SerialUSB.print("Feed id: "); SerialUSB.println(id);
   }
 }
 
